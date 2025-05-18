812c0c12d07c68aa4d10efd85e1de0a09f847fed
Mark Sobkow <mark.sobkow@gmail.com>
2025-05-17 20:07:41 -0600

    Tweaks to CFLib module info for cleanup, and remove the CFDbTest.java that cline had created.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,9 @@
 open module org.msscf.msscf.v2_13.cflib.CFLib {
     requires transitive java.desktop;
     requires transitive org.apache.commons.codec;
-    // requires org.apache.xerces;
     requires transitive jakarta.persistence;
-    // requires jdk.xml.dom;
-    // Ensure Xerces is properly integrated
     exports org.msscf.msscf.v2_13.cflib.CFLib;
     exports org.msscf.msscf.v2_13.cflib.CFLib.xml;
     exports org.msscf.msscf.v2_13.cflib.CFLib.dbutil;
     exports org.msscf.msscf.v2_13.cflib.CFLib.Tip;
-    
-    // Verify that the Xerces version matches your project requirements
 }
