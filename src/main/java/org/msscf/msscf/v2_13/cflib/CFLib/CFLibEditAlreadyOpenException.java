f798a41ef7d1b4ca39fa8163869593f27b7f6279
Mark Sobkow <mark.sobkow@gmail.com>
2025-12-10 01:28:08 -0600

    2025-12-10 01:30 eod

---
+++ src/main/java/org/msscf/msscf/v2_13/cflib/CFLib/CFLibEditAlreadyOpenException.java
@@ -0,0 +1,149 @@
+/*
+ *	MSS Code Factory CFLib 2.13 Common Application Functionality
+ *
+ *	Copyright (C) 2016-2025 Mark Stephen Sobkow (mailto:mark.sobkow@gmail.com)
+ *	
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *	
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *	
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see &lt;https://www.gnu.org/licenses/&gt;.
+ *	
+ *	If you wish to modify and use this code without publishing your changes,
+ *	or integrate it with proprietary code, please contact Mark Stephen Sobkow
+ *	for a commercial license at mark.sobkow@gmail.com
+ */
+
+package org.msscf.msscf.v2_13.cflib.CFLib;
+
+public class CFLibEditAlreadyOpenException extends IllegalStateException {
+
+	public CFLibEditAlreadyOpenException(
+		String msg )
+	{
+		super( msg );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		Class throwingClass,
+		String methName,
+		String msg )
+	{
+		super( throwingClass.getName()
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() "
+					+ ( ( ( msg != null ) && ( msg.length() > 0 ) )
+						? msg
+						: "" ) );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		Class throwingClass,
+		String methName,
+		String msg,
+		Throwable th )
+	{
+		super( throwingClass.getName()
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() "
+					+ ( ( ( msg != null ) && ( msg.length() > 0 ) )
+						? msg
+						: "" ),
+				th );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		String fieldName,
+		String msg )
+	{
+		super( fieldName + "() "
+					+ ( ( ( msg != null ) && ( msg.length() > 0 ) )
+						? msg
+						: "" ) );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		String fieldName,
+		String methName,
+		String msg )
+	{
+		super( fieldName
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() "
+					+ ( ( ( msg != null ) && ( msg.length() > 0 ) )
+						? msg
+						: "" ) );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		String fieldName,
+		String methName,
+		String msg,
+		Throwable th )
+	{
+		super( fieldName
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() "
+					+ ( ( ( msg != null ) && ( msg.length() > 0 ) )
+						? msg
+						: "" ),
+				th );
+	}
+
+	public CFLibEditAlreadyOpenException()
+	{
+		super( "Edit is already open" );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		Class throwingClass,
+		String methName )
+	{
+		super( throwingClass.getName()
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() Edit is already open" );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		Class throwingClass,
+		String methName,
+		Throwable th )
+	{
+		super( throwingClass.getName()
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() Edit is already open",
+				th );
+	}
+
+	public CFLibEditAlreadyOpenException(
+		String fieldName,
+		String methName,
+		Throwable th )
+	{
+		super( fieldName
+					+ ( ( ( methName != null ) && ( methName.length() > 0 ))
+						? "." + methName
+						: "" )
+					+ "() Edit is already open",
+				th );
+	}
+}
